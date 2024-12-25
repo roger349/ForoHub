9266a9cf87695f1d78d0828879f84754074e1228
rogerE.roldan <roger349@gmail.com>
2024-12-25 19:39:40 -0300

    decimo octavo commit appForoHub

---
+++ ForoHub/src/test/java/com/rer/ForoHub/ForoHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rer.ForoHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForoHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
