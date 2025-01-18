d3d8d668332c474f91322028377cd43d52e621d6
rogerE.roldan <roger349@gmail.com>
2025-01-18 15:58:18 -0300

    vigesimo commit appForoHub

--- ForoHub/src/main/resources/static/js/LoginFunciones.js
+++ ForoHub/src/main/resources/static/js/LoginFunciones.js
@@ -1,46 +1,7 @@
-$(document).ready(function(){
-
-    $(".contenedor-formularios").find("input, textarea").on("keyup blur focus", function (e) {
-
-        var $this = $(this),
-        label = $this.prev("label");
-
-        if (e.type === "keyup") {
-            if ($this.val() === "") {
-                label.removeClass("active highlight");
-            } else {
-                label.addClass("active highlight");
-            }
-        } else if (e.type === "blur") {
-            if($this.val() === "") {
-                label.removeClass("active highlight");
-            } else {
-                label.removeClass("highlight");
-            }
-        } else if (e.type === "focus") {
-            if($this.val() === "") {
-                label.removeClass("highlight");
-            }
-            else if($this.val() !== "") {
-                label.addClass("highlight");
-            }
-        }
-
-    });
-
-    $(".tab a").on("click", function (e) {
-
-        e.preventDefault();
-
-        $(this).parent().addClass("active");
-        $(this).parent().siblings().removeClass("active");
-
-        target = $(this).attr("href");
-
-        $(".contenido-tab > div").not(target).hide();
-
-        $(target).fadeIn(600);
-
-    });
-
-});
+// Función para abrir la ventana emergente
+function abrirLogin() {
+    window.open('index.html', 'popup', 'width=400,height=500');
+}
+function abrirRegistrarUsuario() {
+    window.open('registrarUsuario.html', 'popup','status=no, width=400,height=500,toolbar=no,menubar=no,scrollbars=no,resizable=no');
+}
